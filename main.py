bc712ae27e43fcb8a2e2dbbe32fbd2fc51e9cc29
Matthew Clifford <matthewclifford@hotmail.co.uk>
2025-03-22 13:49:13 +0000

    update

--- main.py
+++ main.py
@@ -1,68 +1,69 @@
-import re
-from importlib.metadata import version
 import tiktoken
-print(f"tiktoken version: {version('tiktoken')}")
 
-# 1 TOKENIZE THE SOURCE TEXT 
-# Get the text and assign it to a variable
+# with open("the-verdict.txt", "r", encoding="utf-8") as f:
+#     raw_text = f.read()
+
+# tokenizer = tiktoken.get_encoding("gpt2")
+
+# enc_text = tokenizer.encode(raw_text)
+# enc_sample = enc_text[50:]
+
+# context_size = 4
+# x = enc_sample[:context_size]
+# y = enc_sample[1:context_size+1]
+# print(f"x: {x}")
+# print(f"y:  {y}")
+
+# for i in range(1, context_size+1):
+#     context = enc_sample[:i]
+#     desired = enc_sample[i]
+#     print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
+
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(txt)
+
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+    
+def create_dataloader_v1(txt, batch_size=4, max_length=256, 
+                            stride=128, shuffle=True, drop_last=True,
+                            num_wokers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset =  GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_wokers
+    )
+    return dataloader
+
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
-# Split the words and punctuation, and remove whitespace
-preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
-preprocessed = [item.strip() for item in preprocessed if item.strip()]
-# Sort words by alphabetical order and remove duplicates, then add in text separator and unknown handler
-all_tokens = sorted(set(preprocessed))
-all_tokens.extend(["<|endoftext|>", "<|unk|>"])
-# Create a dictionary of vocabulary and an id value, which is an integer
-vocab = {token:integer for integer,token in enumerate(all_tokens)}
 
-# 2 TOKENIZE AND ENCODE/DECODE GIVEN TEXT
-# Create the tokenizer class
-class SimpleTokenizerv2:
-    def __init__(self, vocab):
-        self.str_to_int = vocab # Vocab:ID as attribute for encode and decode methods
-        self.int_to_str = {i:s for s,i in vocab.items()} # Inverted ID:Vocab 
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+print(first_batch)
 
-    def encode(self, text):
-        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
-        preprocessed = [item.strip() for item in preprocessed if item.strip()]
-        preprocessed = [item if item in self.str_to_int else "<|unk|>" for item in preprocessed]
-        ids = [self.str_to_int[s] for s in preprocessed]
-        return ids
-
-    def decode(self, ids):
-        text = " ".join([self.int_to_str[i] for i in ids])
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
-        return text
-    
-# tokenizer = SimpleTokenizerv2(vocab=vocab)
-# text = """"It's the last he painted, you know," Mrs. Gisburn said with pardonable pride."""
-# ids = tokenizer.encode(text)
-# print(ids)
-# print(tokenizer.decode(ids))
-
-text1 = "Hello, do you like tea?"
-text2 = "In the sunlit terraces of the palace."
-text = " <|endoftext|> ".join((text1, text2))
-
-# tokenizer = SimpleTokenizerv2(vocab=vocab)
-# print(tokenizer.encode(text))
-# print(tokenizer.decode(tokenizer.encode(text)))
-
-tokenizer = tiktoken.get_encoding("gpt2")
-text = (
-    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
-    "of someunknownPlace"
-)
-# integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-# print(integers)
-# strings = tokenizer.decode(integers)
-# print(strings)
-
-
-unknownwords = "Akwirw ier"
-integers = tokenizer.encode(unknownwords)
-print(integers)
-
-strings = tokenizer.decode(integers)
-print(strings)
+second_batch = next(data_iter)
+print(second_batch)

e820f361805621606853fdf5905320c3437f08fb
Matthew Clifford <matthewclifford@hotmail.co.uk>
2025-03-25 19:36:13 +0000

    created summariser of websites using beautiful soup and ollama

--- main.py
+++ main.py
@@ -1,69 +1,20 @@
-import tiktoken
+import requests
+from bs4 import BeautifulSoup
+from IPython.display import Markdown, display
 
-# with open("the-verdict.txt", "r", encoding="utf-8") as f:
-#     raw_text = f.read()
+OLLAMA_API = "http://localhost:11434/api/chat"
+HEADERS = {"Content-Type": "application/json"}
+MODEL = "llama3.2"
 
-# tokenizer = tiktoken.get_encoding("gpt2")
+messages = [
+    {"role": "user", "content": "Describe some business application of Generative AI"}
+]
 
-# enc_text = tokenizer.encode(raw_text)
-# enc_sample = enc_text[50:]
+payload = {
+    "model": MODEL,
+    "messages": messages,
+    "stream": False
+}
 
-# context_size = 4
-# x = enc_sample[:context_size]
-# y = enc_sample[1:context_size+1]
-# print(f"x: {x}")
-# print(f"y:  {y}")
-
-# for i in range(1, context_size+1):
-#     context = enc_sample[:i]
-#     desired = enc_sample[i]
-#     print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
-
-
-import torch
-from torch.utils.data import Dataset, DataLoader
-
-class GPTDatasetV1(Dataset):
-    def __init__(self, txt, tokenizer, max_length, stride):
-        self.input_ids = []
-        self.target_ids = []
-
-        token_ids = tokenizer.encode(txt)
-
-        for i in range(0, len(token_ids) - max_length, stride):
-            input_chunk = token_ids[i:i + max_length]
-            target_chunk = token_ids[i + 1: i + max_length + 1]
-            self.input_ids.append(torch.tensor(input_chunk))
-            self.target_ids.append(torch.tensor(target_chunk))
-
-    def __len__(self):
-        return len(self.input_ids)
-    
-    def __getitem__(self, idx):
-        return self.input_ids[idx], self.target_ids[idx]
-    
-def create_dataloader_v1(txt, batch_size=4, max_length=256, 
-                            stride=128, shuffle=True, drop_last=True,
-                            num_wokers=0):
-    tokenizer = tiktoken.get_encoding("gpt2")
-    dataset =  GPTDatasetV1(txt, tokenizer, max_length, stride)
-    dataloader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=shuffle,
-        drop_last=drop_last,
-        num_workers=num_wokers
-    )
-    return dataloader
-
-with open("the-verdict.txt", "r", encoding="utf-8") as f:
-    raw_text = f.read()
-
-dataloader = create_dataloader_v1(
-    raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
-data_iter = iter(dataloader)
-first_batch = next(data_iter)
-print(first_batch)
-
-second_batch = next(data_iter)
-print(second_batch)
+response = requests.post(OLLAMA_API, json=payload, headers=HEADERS)
+print(response.json()["message"]["content"])

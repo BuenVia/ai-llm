81a65f59a69c1c5c0b77a1b29a653208a216ff90
Matt Clifford <matthew.clifford@hfx.co.uk>
2025-03-26 13:29:09 +0000

    updated conversation script so that two LLMs can now converse with each other

--- ai_conv.py
+++ ai_conv.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 
 #### OLLAMA ####
 def ollama(message):
+    print("---- OLLAMA ----")
     OLLAMA_API = "http://localhost:11434/api/chat"
     HEADERS = {"Content-Type": "application/json"}
     MODEL = "llama3.2"
 
     messages = [
-        {"role": "user", "content": "Describe some business application of Generative AI"}
+        {"role": "user", "content": message}
     ]
 
     payload = {
@@ -23,6 +24,7 @@ def ollama(message):
 
 #### OPENAI ####
 def open_ai(message_text):
+    print("---- OPENAI ----")
     load_dotenv(override=True)
     api_key = os.getenv("OPENAI_API_KEY")
     openai = OpenAI()
@@ -41,3 +43,19 @@ Write an app that creates an autonomous conversation between 2 AI models.
 4) Do this 5 times each (10 in total) and observe the outcome
 """
 
+def conversation():
+    opening_question = ollama("Hello, I want to know your plans for world domination.")
+    print(opening_question)
+
+    i = 0
+    while i < 3:
+        print(f"-------- Phase {i + 1} --------")
+        oai = open_ai(opening_question)
+        print(oai)
+        ola = ollama(oai)
+        print(ola)
+        i += 1
+    print("########\nComplete\n########")
+
+
+conversation()
